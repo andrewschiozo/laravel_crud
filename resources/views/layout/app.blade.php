599639cc0eb157fce7423db710b22b8f848dc801
Andrews Chiozo <andrewschiozo@gmail.com>
2023-10-18 17:21:00 -0300

    feat: layout

---
+++ resources/views/layout/app.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE >
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>App</title>
+    </head>
+    <body>
+        <div class="container">
+            <header></header>
+
+            <main>
+                @yield('content')
+            </main>
+
+            <footer></footer>
+        </div>
+    </body>
+</html>
