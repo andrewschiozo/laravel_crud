7ccdbe8c34c2f5c4d930b6fdc3985a8187d5c28f
Andrews Chiozo <andrewschiozo@gmail.com>
2023-10-18 15:06:39 -0300

    feat: view produto por id

---
+++ resources/views/produtos/show.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ver um produto</title>
+</head>
+<body>
+    <label for="">Nome</label> <br />
+    <input type="text" name="nome" value="{{$produto->nome}}"> <br />
+
+    <label for="">Custo</label> <br />
+    <input type="text" name="custo" value="{{$produto->custo}}"> <br />
+
+    <label for="">Preco</label> <br />
+    <input type="text" name="preco" value="{{$produto->preco}}"> <br />
+
+    <label for="">quantidade</label> <br />
+    <input type="text" name="quantidade" value="{{$produto->quantidade}}"> <br />
+</body>
+</html>
