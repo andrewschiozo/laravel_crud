52a8553dcf6e05bd0727f105c859d9201502e1b1
Andrews Chiozo <andrews.chiozo@cggn.com.br>
2023-10-18 11:26:11 -0300

    feat: form produto

---
+++ resources/views/produtos/create.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cadastrar um novo produto</title>
+</head>
+<body>
+    <form action="">
+        <label for="">Nome</label> <br />
+        <input type="text" name="nome"> <br />
+
+        <label for="">Custo</label> <br />
+        <input type="text" name="custo"> <br />
+
+        <label for="">Preco</label> <br />
+        <input type="text" name="preco"> <br />
+
+        <label for="">quantidade</label> <br />
+        <input type="text" name="quantidade"> <br />
+
+        <button>Salvar</button>
+    </form>
+</body>
+</html>
