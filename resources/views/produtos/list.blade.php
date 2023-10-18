79841b166aaf1df946b3a4ca34978680b90e0010
Andrews Chiozo <andrewschiozo@gmail.com>
2023-10-18 17:29:48 -0300

    fix: links novo/editar/exluir

--- resources/views/produtos/list.blade.php
+++ resources/views/produtos/list.blade.php
@@ -2,7 +2,7 @@
 @section('content')
 
     <h2>Produtos</h2>
-    <a href="#">Novo produto</a>
+    <a href="/produto/novo">Novo produto</a>
     <table>
         <thead>
             <tr>
@@ -26,7 +26,7 @@
                 <td>{{$p->quantidade}}</td>
                 <td>{{$p->created_at}}</td>
                 <td>{{$p->updated_at}}</td>
-                <td><a href="#">Editar</a> <a href="#">Excluir</a></td>
+                <td><a href="/produto/editar/{{$p->id}}">Editar</a> <a href="/produto/excluir/{{$p->id}}">Excluir</a></td>
             </tr>
             @endforeach
         </tbody>
