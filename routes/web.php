82910a5862481efe27017e7fa4555ed34f6f580f
Andrews Chiozo <andrewschiozo@gmail.com>
2023-10-24 17:23:16 -0300

    feat: rota index categoria

--- routes/web.php
+++ routes/web.php
@@ -17,8 +17,12 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+// Produto
 Route::get('/produto', [App\Http\Controllers\ProdutosController::class, 'index']);
 Route::get('/produto/get/{id?}', [App\Http\Controllers\ProdutosController::class, 'get']);
 Route::post('/produto/novo', [App\Http\Controllers\ProdutosController::class, 'store']);
 Route::post('/produto/editar/{id}', [App\Http\Controllers\ProdutosController::class, 'update']);
 Route::post('/produto/excluir/{id}', [App\Http\Controllers\ProdutosController::class,'destroy']);
+
+// Categoria
+Route::get('/categoria', [App\Http\Controllers\CategoriasController::class, 'index']);
