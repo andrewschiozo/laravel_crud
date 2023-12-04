f7a5f18c5cfc26de04f2376d0f0b1bad99baf553
Andrews Chiozo <andrewschiozo@gmail.com>
2023-12-04 16:27:38 -0300

    feat: autorizacao com Laravel Breeze

--- routes/web.php
+++ routes/web.php
@@ -17,16 +17,29 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-// Produto
-Route::get('/produto', [App\Http\Controllers\ProdutosController::class, 'index']);
-Route::get('/produto/get/{id?}', [App\Http\Controllers\ProdutosController::class, 'get']);
-Route::post('/produto/novo', [App\Http\Controllers\ProdutosController::class, 'store']);
-Route::post('/produto/editar/{id}', [App\Http\Controllers\ProdutosController::class, 'update']);
-Route::post('/produto/excluir/{id}', [App\Http\Controllers\ProdutosController::class,'destroy']);
+// Auth
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-// Categoria
-Route::get('/categoria', [App\Http\Controllers\CategoriasController::class, 'index']);
-Route::get('/categoria/get/{id?}', [App\Http\Controllers\CategoriasController::class, 'get']);
-Route::post('/categoria/novo', [App\Http\Controllers\CategoriasController::class, 'store']);
-Route::post('/categoria/editar/{id}', [App\Http\Controllers\CategoriasController::class, 'update']);
-Route::post('/categoria/excluir/{id}', [App\Http\Controllers\CategoriasController::class,'destroy']);
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [App\Http\Controllers\ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [App\Http\Controllers\ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [App\Http\Controllers\ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    // Produto
+    Route::get('/produto', [App\Http\Controllers\ProdutosController::class, 'index']);
+    Route::get('/produto/get/{id?}', [App\Http\Controllers\ProdutosController::class, 'get']);
+    Route::post('/produto/novo', [App\Http\Controllers\ProdutosController::class, 'store']);
+    Route::post('/produto/editar/{id}', [App\Http\Controllers\ProdutosController::class, 'update']);
+    Route::post('/produto/excluir/{id}', [App\Http\Controllers\ProdutosController::class,'destroy']);
+
+    // Categoria
+    Route::get('/categoria', [App\Http\Controllers\CategoriasController::class, 'index']);
+    Route::get('/categoria/get/{id?}', [App\Http\Controllers\CategoriasController::class, 'get']);
+    Route::post('/categoria/novo', [App\Http\Controllers\CategoriasController::class, 'store']);
+    Route::post('/categoria/editar/{id}', [App\Http\Controllers\CategoriasController::class, 'update']);
+    Route::post('/categoria/excluir/{id}', [App\Http\Controllers\CategoriasController::class,'destroy']);
+});
+
+require __DIR__.'/auth.php';
